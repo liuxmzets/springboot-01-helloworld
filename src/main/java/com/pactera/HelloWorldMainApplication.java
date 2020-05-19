fac86a8dd9b783309239e0abc69a7af84ff400e0
liuxmzets <liuxmzets@163.com>
2020-05-19 16:31:07 +0800

    Initial commit

---
+++ src/main/java/com/pactera/HelloWorldMainApplication.java
@@ -0,0 +1,17 @@
+package com.pactera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @SpringBootApplication
+ * 来标注一个主程序类，
+ * 说明这是一个Spring Boot应用
+ */
+@SpringBootApplication
+public class HelloWorldMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldMainApplication.class,
+                args);
+    }
+}
